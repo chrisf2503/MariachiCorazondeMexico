ca214252879afa93f92637c359f0cca41ae6d554
Christopher Flores <Christopher321flores@gmail.com>
2026-03-01 07:43:38 -0500

    adding about us page to work on later

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import { BrowserRouter,Routes, Route } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 // import App from './pages/App.jsx'
 import Home from './pages/home.jsx'
+import AboutUs from './pages/aboutus.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       {/* <Route path="/" element={<App/>}/> */}
       <Route path='/' element={<Home/>}/>
-      <></>
+      <Route path='/aboutus' element={<AboutUs/>}/>
     </Routes>
   </BrowserRouter>
 
---
+++ src/pages/aboutus.jsx
@@ -0,0 +1,15 @@
+import NavBar from './components/navBar';
+import Footer from './components/footer';
+function AboutUs(){
+    return (
+        <>
+            <NavBar/>
+            <main className="aboutusContent">
+                
+            </main>
+            <Footer/>
+        </>
+    );
+}
+
+export default AboutUs;
